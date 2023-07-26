c1f6ad89c6b44ccf9b6eb61cb8f93d81639a94bf
Tejaswanth <2058830@cognizant.com>
2023-07-26 11:40:23 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name = 'demo-project',
+    version = '0.0.1',
+    author = 'Tejaswanth',
+    author_email = 'mucheli.n.tejaswanth1@gmail.com',
+    packages = find_packages(),
+    install_requires = ['pandas', 'numpy', 'flask']
+    )
